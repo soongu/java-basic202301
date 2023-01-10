211ba8fe02b68a1457c59c928683a07da400f2c7
soongu <naong@naver.com>
2023-01-10 15:50:07 +0900

    # 상속 예제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,25 @@
 package org.example;
 
 import org.example.basic.Dancer;
+import org.example.inherit.IdolDancer;
+import org.example.inherit.StreetDancer;
 
 public class Main {
     public static void main(String[] args) {
 
-        Dancer dancer = new Dancer("총알탄소년단", "춤신춤왕");
-        dancer.dance();
+
+        IdolDancer idolDancer
+                = new IdolDancer("AB6IX", "이대휘");
+
+        StreetDancer streetDancer
+                = new StreetDancer("길거리춤꾼들", "박격포");
+
+        idolDancer.dance();
+        System.out.println();
+
+        streetDancer.dance();
+
+
 
     }
 }
---
+++ src/main/java/org/example/inherit/IdolDancer.java
@@ -0,0 +1,34 @@
+package org.example.inherit;
+
+import org.example.basic.Dancer;
+
+public class IdolDancer extends Dancer {
+
+    private int numberOfFan; // 팬의 수
+
+    // 생성자를 하나도 선언하지 않으면 기본 생성자가 자동으로 만들어집니다.
+    public IdolDancer(String crewName, String myName) {
+        super(crewName, myName);
+        this.numberOfFan = 10;
+        System.out.println("idol dancer 생성됨!");
+    }
+
+    // 오버라이딩: 부모의 메서드를 재정의
+    // 부모의 메서드 시그니처(이름, 리턴타입, 파라미터)를 똑같이 맞춰야함
+    @Override
+    public void dance() {
+        super.dance();
+        wink();
+    }
+
+    private void wink() {
+        System.out.println("윙크를 세게 갈깁니다.");
+    }
+}
+
+
+
+
+
+
+

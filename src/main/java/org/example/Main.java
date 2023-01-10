d452c30ec60fb0e843a1e13095927a484f2e3f8b
soongu <naong@naver.com>
2023-01-10 16:06:16 +0900

    # 다형성 예제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,22 +3,27 @@ package org.example;
 import org.example.basic.Dancer;
 import org.example.inherit.IdolDancer;
 import org.example.inherit.StreetDancer;
+import org.example.poly.PerformanceTeam;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        IdolDancer idolDancer
-                = new IdolDancer("AB6IX", "이대휘");
 
-        StreetDancer streetDancer
-                = new StreetDancer("길거리춤꾼들", "박격포");
+        Dancer idolDancer1 = new IdolDancer("아이돌팀1", "아이돌1");
+        Dancer idolDancer2 = new IdolDancer("아이돌팀2", "아이돌2");
+        Dancer idolDancer3 = new IdolDancer("아이돌팀3", "아이돌3");
 
-        idolDancer.dance();
-        System.out.println();
+        Dancer streetDancer1 = new StreetDancer("스트릿팀1", "스트릿댄서1");
+        Dancer streetDancer2 = new StreetDancer("스트릿팀2", "스트릿댄서2");
+        Dancer streetDancer3 = new StreetDancer("스트릿팀3", "스트릿댄서3");
 
-        streetDancer.dance();
+        PerformanceTeam team = new PerformanceTeam();
 
+        team.joinDancer(idolDancer1);
+        team.joinDancer(streetDancer3);
+        team.joinDancer(streetDancer2);
+        team.joinDancer(idolDancer3);
 
 
     }
